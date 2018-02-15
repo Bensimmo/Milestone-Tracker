97d6e0cd10d5c7ca71d79af1688a513907366750
Jake Sergeant <jakesergeant@Jakes-MacBook-Pro.local>
2018-02-15 21:22:51 +1000

    Basic sidebar setup

---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <div id="sidebar">
+                <ul>
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                    <li><a href="#">Example Link</a></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
